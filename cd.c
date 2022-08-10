501356a222dd8229bdd9477202af0fff414045e8
Joseph Ochieng Kamau <ochiengkamauj@gmail.com>
2022-08-10 12:40:51 -0700

    Fixed bug in cd.c

--- cd.c
+++ cd.c
@@ -72,6 +72,9 @@ void cd_to(data_shell *datash)
 	cp_pwd = _strdup(pwd);
 	set_env("OLDPWD", cp_pwd, datash);
 
+	cp_dir = _strdup(dir);
+	set_env("PWD", cp_dir, datash);
+
 	free(cp_pwd);
 	free(cp_dir);
 
